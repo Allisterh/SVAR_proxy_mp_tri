411bfc5eb723c6c504f68e70344d34d886822f54
swang1112 <59112676+swang1112@users.noreply.github.com>
2020-08-30 22:10:46 +0200

    partial historical decomposition of proxy SVARs

--- Applications/HD.R
+++ Applications/HD.R
@@ -25,20 +25,21 @@ hd_pi_mp <- hd_pi_SR_mp %>% left_join(hd_pi_dCov_mp, by = "t") %>%
   left_join(hd_pi_CV_mp, by = "t") %>% left_join(hd_pi_SW_mp, by = "t") %>% 
   left_join(hd_pi_SZ_mp, by = "t") %>% left_join(hd_pi_RR_mp, by = "t") %>% tidyr::drop_na()
 
-hd_pi_mp %<>% dplyr::filter(t >= 1970 & t <= 1985) 
+hd_pi_mp %<>% dplyr::filter(t >= 1973 & t <= 1983) 
 
 colnames(hd_pi_mp)[-1] <- c("SR", "dCov", "CV", "SW", "SZ", "RR")
 
-recessions_start <- c(1969.75, 1973.75, 1980)
-recessions_end <- c(1970.75, 1975, 1982.75)
+recessions_start <- c( 1973.75, 1980,1981.5)
+recessions_end <- c( 1975, 1980.5,1982.75)
 
 hd_pi_mp_plot <- hd_pi_mp %>% reshape2::melt(id = "t") %>% ggplot(aes(x = t, y = value)) +
   geom_line() + facet_wrap(~variable, ncol = 3) + xlab("") + ylab("") + 
   theme_bw() + geom_hline(yintercept = 0, color = "red", alpha = 0.5) +
+  scale_x_continuous(breaks = seq(hd_pi_mp$t[1] %>% ceiling(),hd_pi_mp$t[nrow(hd_pi_mp)], by = 2)) +  
   annotate("rect", xmin = recessions_start, xmax = recessions_end, ymin = -Inf, ymax = Inf, alpha = 0.3)
+ggsave("../Plots/hd_pi_mp.pdf", plot = hd_pi_mp_plot, width = 12, height = 6)
 
-
-# x ----------------------------------------------------------------------
+ # x ----------------------------------------------------------------------
 hd_x_SR <- hd.mod(SR, series = 1, Partial = NULL, Epsname)
 hd_x_SR_mp <- hd.mod(SR, series = 1, Partial = 3, Epsname)
 hd_x_CV <- hd.mod(CV, series = 1, Partial = NULL, Epsname)
